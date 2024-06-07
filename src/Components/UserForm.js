4764b2a36c5ef2b732403fdc682a1c69e7989d48
SupunTJ <jayaweeraspn@gmail.com>
2024-06-07 10:00:00 +0530

    setting up the project

---
+++ src/Components/UserForm.js
@@ -0,0 +1,16 @@
+import { Grid } from "@mui/material";
+import React from "react";
+
+const UserForm = (props) => {
+  return (
+    <Grid
+      container
+      spacing={2}
+      sx={{ backgroundColor: "white", marginBottom: "30px", display: "block" }}
+    >
+      <Grid></Grid>
+    </Grid>
+  );
+};
+
+export default UserForm;
